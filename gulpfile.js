b231015b10dbaf558987545941c56430bd1ade2a
Denisiums <denisiums@gmail.com>
2018-01-26 00:23:04 +0300

    scss installed

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task('default', defaultTask);
 
-function defaultTask(done) {
-  // place code for your default task here
-  done();
-}
+gulp.task('styles', function() {
+  gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+  gulp.watch('sass/**/*.scss',['styles']);
+});
