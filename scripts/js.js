a1d8a6608eda9ea169f861488fb2ad9800b3210e
Denisiums <denisiums@gmail.com>
2018-01-29 02:17:08 +0300

    layout gallery

--- scripts/js.js
+++ scripts/js.js
@@ -1,1 +1,37 @@
-console.log('gali');
+$( document ).ready(function() {
+  console.log( 'ready!' );
+  console.log( '$:', $ );
+  console.log( '.responsive', $('.gallery') );
+  $('.gallery').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    gallery: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
